d9b5881ed9f40619bfb20497ffab26350e48d877
vchikalkin <djchikalkin@gmail.com>
2026-07-08 21:00:58 +0300

    remove dead code

--- components/sections/home-lab-section.tsx
+++
@@ -1,8 +0,0 @@
-import { getTranslations } from 'next-intl/server';
-import { HomeLabContent } from '@/components/sections/home-lab-content';
-
-export async function HomeLabSection() {
-  const t = await getTranslations('HomeLab');
-
-  return <HomeLabContent title={t('title')} description={t('description')} />;
-}
